21cbac9356f13b234cdcb3ec19f643af221c5d1a
muhem <erwinsyahrul9@gmail.com>
2020-06-12 00:05:53 +0700

    1

---
+++ OOP-Abstrac/OOP-Abstrac/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace OOP_Abstrac
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Implementasi imple = new Implementasi();
+			string nama = "Muhammad Erwin";
+			string almt = "Yogyakarta";
+			string NIM = "2687";
+			string Jurusan = "Informatika";
+
+			imple.ShowKTP(nama, almt);
+			imple.ShowMHS(nama, NIM, Jurusan);
+
+			Console.ReadKey(true);
+		}
+	}
+}
